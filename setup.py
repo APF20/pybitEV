f6f93f4926837ce08251ca2d1fa800515fdeea73
APF20 <74583612+APF20@users.noreply.github.com>
2022-01-25 17:28:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pybitEV',
-    version='3.0.1',
+    version='3.3.2',
     description='Python3 Bybit Asyncio HTTP/WebSocket API Connector', 
     long_description=long_description,
     long_description_content_type='text/markdown',
